f962775759186b723603cae4a18d849b85cc8e15
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2019-04-23 09:51:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -348,8 +348,6 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`!p | Music bot`,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
 });
 
 client.login(process.env.BOT_TOKEN);
