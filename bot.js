c5b38cf7ac0b4d8806b07aa11e210a51033df38d
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2019-04-23 09:48:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "!";
+const prefix = "Mup";
 /////////////////////////
 ////////////////////////
 client.on('message', async msg =>{
